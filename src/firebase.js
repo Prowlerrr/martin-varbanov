931faf07fecb6891d0e42a844cc8efd84e0130d2
evarbanov <evarbanov@minervanetworks.com>
2024-02-08 15:29:18 +0200

    Fix firestore

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,25 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_MV_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_MV_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_MV_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_MV_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MV_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_MV_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_MV_FIREBASE_MEASUREMENT_ID,
+    apiKey: "AIzaSyB3MBpAKcn0w_C5EW4HCDF0T8IKF7vntps",
+    authDomain: "martin-varbanov.firebaseapp.com",
+    projectId: "martin-varbanov",
+    storageBucket: "martin-varbanov.appspot.com",
+    messagingSenderId: "478850414000",
+    appId: "1:478850414000:web:2ec3d38a2d5894de4c9275",
+    measurementId: "G-7692YCR5BV"
 };
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_MV_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_MV_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_MV_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_MV_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MV_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_MV_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_MV_FIREBASE_MEASUREMENT_ID,
+// };
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
