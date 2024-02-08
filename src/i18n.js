f75be1f5f8f4f3db617de08c4f1179f8e930c5bd
evarbanov <evarbanov@minervanetworks.com>
2024-02-08 09:32:38 +0200

    Fix locales

--- src/i18n.js
+++ src/i18n.js
@@ -2,14 +2,23 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translationBG from './locales/bg/translation.json';
+import translationEN from './locales/en/translation.json';
 
-// It might be that you need the translations loaded from .json files
-// For this case use i18next-http-backend
+
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .use(HttpBackend)
     .use(LanguageDetector)
     .init({
+        resources: {
+            en: {
+                translation: translationEN
+            },
+            bg: {
+                translation: translationBG
+            }
+        },
         lng: "bg",
         fallbackLng: "bg", // use bg if detected lng is not available
         detection: {
@@ -17,7 +26,7 @@ i18n
             caches: ['cookie']
         },
         interpolation: {
-            escapeValue: false // not needed for react as it escapes by default
+            escapeValue: false
         }
     });
 
