909f197114bfc0c6fbd57aec226529d29b8649d6
evarbanov <evarbanov@minervanetworks.com>
2024-02-27 12:59:32 +0200

    Upload new image in group

--- src/components/AchievementsGallery/NewAchievement.tsx
+++ src/components/AchievementsGallery/NewAchievement.tsx
@@ -13,6 +13,7 @@ const NewAchievement: React.FC = () => {
 
     const [imageUpload, setImageUpload] = useState(null);
     const [imageUrl, setImageUrl] = useState(null);
+    const [imageUploadDone, setImageUploadDone] = useState(false);
 
     const uploadImage = () => {
         if (imageUpload == null) return
@@ -26,6 +27,13 @@ const NewAchievement: React.FC = () => {
             .catch(error => console.error('Error uploading file:', error));
     }
 
+    const uploadNewImageInGroup = () => {
+        setImageDescription('')
+        setImageUpload(null)
+        setImageUrl(null)
+        setImageUploadDone(false)
+    }
+
     const uploadMetadata = async (imageUrl: string | null) => {
         if (selectedGroup == null) return
         if (imageDescription == null) return
@@ -37,13 +45,13 @@ const NewAchievement: React.FC = () => {
                 description: selectedGroup.description
             });
         }
-        const docRef = await addDoc(
+        await addDoc(
             collection(firestore, 'images', 'achievements', selectedGroup.name),
             {
                 url: imageUrl,
                 description: imageDescription
             }
-        );
+        ).then(() => setImageUploadDone(true));
     }
 
     // @ts-ignore
@@ -77,6 +85,7 @@ const NewAchievement: React.FC = () => {
                         <Col sm="6">
                             <input
                                 type="file"
+                                disabled={imageUploadDone}
                                 onChange={(event) => {
                                     // @ts-ignore
                                     const f = event.target.files[0];
@@ -102,6 +111,7 @@ const NewAchievement: React.FC = () => {
                             <Form.Group>
                                 <Form.Label>Описание</Form.Label>
                                 <Form.Control
+                                    disabled={imageUploadDone}
                                     type="text"
                                     value={imageDescription}
                                     onChange={(e) => setImageDescription(e.target.value)}
@@ -110,7 +120,13 @@ const NewAchievement: React.FC = () => {
                             </Form.Group>
                         </Col>
                         <Row> <Col className="border-top my-3"></Col> </Row>
-                        <Button variant="primary" className="mt-2" onClick={uploadImage} disabled={!imageDescription}>Добави</Button>
+                        {
+                            imageUploadDone
+                            ?
+                            <Button variant="primary" className="mt-2" onClick={uploadNewImageInGroup}>Добави нова картинка в категорията</Button>
+                            :
+                            <Button variant="primary" className="mt-2" onClick={uploadImage} disabled={!imageDescription}>Добави</Button>
+                        }
                     </Row>
                 </div>
                 :
